604150b3d76b8b2710af8936d52a7fdf66295e78
Tapiceria Bohemia <tapiceriabohemia@gmail.com>
2018-10-08 09:00:02 +0200

    Refactor slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,23 +1,45 @@
-/* eslint-disable */
-
 const header = document.querySelector('header');
 const container = document.querySelector('header .container');
+const TIME_BETWEEN_IMAGES = 6000;
+const TIME_IMAGE_TRANSITION = 1500;
 
-const toImage = (imageNumber) => {
-	setTimeout(() => {
-		requestAnimationFrame(() => {
-			container.style.background = 'rgba(0, 0, 0, 1.0)';
-			setTimeout(() => {
-				requestAnimationFrame(() => {
-					container.style.background = 'rgba(0, 0, 0, 0.8)';
-					header.style.backgroundImage = `url(/img/header-bg-${imageNumber}.jpg)`;
-					container.style.background = 'rgba(0, 0, 0, 0.0)';
-					toImage((imageNumber + 1) > 4 ? 0 : imageNumber + 1);
-				});
-			}, 1500);
-		});
-	}, 6000);
+const setBackgroundImageNumber = (imageNumber) => {
+	header.style.backgroundImage = `url(/img/header-bg-${imageNumber}.jpg)`;
+};
+const fadeImage = () => { container.style.background = 'rgba(0, 0, 0, 1.0)'; };
+const displayImage = () => { container.style.background = 'rgba(0, 0, 0, 0.0)'; };
+const getRandomImageNumber = (exclude) => {
+	let random = Math.floor(Math.random() * 5);
+
+	while (random === exclude) { random = Math.floor(Math.random() * 5); }
+
+	return random;
 };
 
-header.style.backgroundImage = `url(/img/header-bg-${Math.floor(Math.random() * 5)}.jpg)`;
-toImage(Math.floor(Math.random() * 5));
+const changeImage = (imageNumber) => {
+	setTimeout(() => {
+		requestAnimationFrame(() => {
+			setBackgroundImageNumber(imageNumber);
+			displayImage();
+			transitionToImage((imageNumber + 1) > 4 ? 0 : imageNumber + 1);//eslint-disable-line no-use-before-define, no-magic-numbers, max-len
+		});
+	}, TIME_IMAGE_TRANSITION);
+};
+
+const transitionToImage = (imageNumber) => {
+	setTimeout(() => {
+		requestAnimationFrame(() => {
+			fadeImage();
+			changeImage(imageNumber);
+		});
+	}, TIME_BETWEEN_IMAGES);
+};
+
+const init = () => {
+	const firstImageNumber = getRandomImageNumber();
+
+	setBackgroundImageNumber(firstImageNumber);
+	transitionToImage(getRandomImageNumber(firstImageNumber));
+};
+
+init();
